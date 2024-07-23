8138839c1b2cbf59ff049e69b750adb49bdd9db6
foxymplayer <gufoxy194@gmail.com>
2024-07-23 12:18:06 -0300

    quase finalizando o projeto

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Ecommerce_loja
+# Indice 
+[⚙️Funcionalidades⚙️]()<>
+[✅Metodos ultilizados✅]()<>
+[🍏Progamas ultilizados🍏]()<>
+[👷Criador👷]()<>
+[📅Datas📅]()<>
+
+## ⚙️Funcionalidades⚙️
+
+## ✅Metodos ultilizados✅
+
+## 🍏Progamas ultilizados🍏
+
+## 👷Criador👷
+
+
+## 📅Datas📅
+Iniciado em: 11/06/2024.
+Finalizado: 23/07/2024.
---
+++ cadEcommerce/marca.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar marca</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="center">
+            <h1>Cadastro de marca</h1>
+            <a href="index.php" target="_self">Voltar</a>
+        </div>
+    </header>
+    <section id="produtos">
+        <form action="insere-marca.php" method="post">
+            <label for="">Isere sua marca:</label>
+            <input type="text" name="descricao">
+            <input type="submit" value="Cadastrar">
+            
+        </form>
+    </section>
+</body>
+</html>
